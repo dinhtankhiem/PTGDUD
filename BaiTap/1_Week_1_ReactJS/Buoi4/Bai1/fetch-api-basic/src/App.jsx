1f1451ce1f6739a100fe28c218ff650a2b230063
H82M19\Student <Student>
2026-03-11 10:46:06 +0700

    Hoan thanh bai 1 buoi 4 React

---
+++ BaiTap/1_Week_1_ReactJS/Buoi4/Bai1/fetch-api-basic/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const url = 'https://jsonplaceholder.typicode.com/users';
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url);
+        console.log(res);
+
+        if (!res.ok) {
+          throw new Error('Error Network');
+        }
+
+        const result = await res.json();
+        console.log(result);
+
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Danh sách Users</h1>
+
+      {
+        loading ? (
+          <p className="loading">Loading...</p>
+        ) : (error == null ? (
+          data.map((item) => {
+            return (
+              <div key={item.id} className="card">
+                <p><b>Name:</b> {item.name}</p>
+                <p><b>Email:</b> {item.email}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p className="error">Error: {error}</p>
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
